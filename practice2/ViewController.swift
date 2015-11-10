0a2bf2edabc45ae479b9b973eac1ce88c526b76b
Chojaeyoung <jjy0484@gmail.com>
2015-11-10 19:57:16 +0900

    Edit program source

--- practice2/ViewController.swift
+++ practice2/ViewController.swift
@@ -11,14 +11,29 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textValue: UILabel!
-    var butImage:UIImage!
+    
+    @IBOutlet weak var viewTitle: UINavigationItem!
+    var number = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        arrowButton.backgroundImageForState()
-        butImage = UIImage(named:"arrow_red.png")
+        textValue.text = String(self.number)
+        viewTitle.title = String(self.number)
+        print("viewDidLoad",number)
+        number++
     }
 
+    @IBAction func buttonArrow(sender: AnyObject) {
+
+    }
+//    
+//    override func viewWillAppear(animated: Bool) {
+//        print("test", number++)
+//        textValue.text = String(self.number)
+//        viewTitle.title = String(self.number)
+//
+//    }
+//    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
